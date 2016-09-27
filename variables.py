edb12492d74bac7863082b7e28b561c73c7d5205
Jean Tu <tujean27@gmail.com>
2016-09-27 08:29:13 -0700

    adding new lesson of variables to Python folder

---
+++ variables.py
@@ -0,0 +1,8 @@
+#This is the first lesson inside CodeAcademy, where we learn how to define variables
+#The interesting thing about Python is htat you don't have to define what it is that you are going to be storing
+#In the other 2 languages that I have learned (Java & C++) we would have to declare the variable
+#char a = 'a'; for example, but with python you just assign it and use it!
+
+my_variable = 10; #automatically sets my_variable to 10
+my_name = "Jean Tu"; #It's cool how we don't have to declare the type
+print my_name; 
