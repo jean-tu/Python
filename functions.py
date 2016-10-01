db5f632d79a7fd882412cf9488cd878ca2094c4d
Jean Tu <tujean27@gmail.com>
2016-10-01 15:20:03 -0700

    added a file to learn on how to declare a function as well as how to call on it

---
+++ functions.py
@@ -0,0 +1,7 @@
+#learning about how to declare functions and be able to use them
+
+def function_name():#declaring a function called function_name with no arguments that are to be taken in within the parenthesis
+    print "Hello this is within the function"
+    pass
+
+function_name() #calls on the function_name() function
