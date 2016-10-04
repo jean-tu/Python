52084897f60ce8b9d728d44008f3799e194b27c5
Jean Tu <tujean27@gmail.com>
2016-10-03 21:01:14 -0700

    adding the new comparisons.py file that has truth table

---
+++ comparisons.py
@@ -0,0 +1,27 @@
+'''this file will go over the different comparison functions that can be used'''
+
+''' This is how you do a multilined comment using the triple apostrophes
+Equal to (==)
+Not equal to (!=)
+Less than (<)
+Less than or equal to (<=)
+Greater than (>)
+Greater than or equal to (>=)
+'''
+
+"""
+     Boolean Operators
+------------------------      True and True is True
+True and False is False
+False and True is False
+False and False is False
+
+True or True is True
+True or False is True
+False or True is True
+False or False is False
+
+Not True is False
+Not False is True
+
+"""
