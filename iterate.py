f4e9a2b60a33a20857846b21ce8e05851085255b
Jean Tu <tujean27@gmail.com>
2016-12-12 12:05:29 -0800

    finding the amount of characters in string.printable

--- iterate.py
+++ iterate.py
@@ -1,9 +1,6 @@
-'''
-string = ['a', b','c','d','e','f','g','h','i','j','k','l','m']
-for var in string:
-    print(var)
-'''
-
-string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,./<>?;:!@#$%^&*()"
-for var in string:
+import string
+string = string.printable
+print str(len(string))
+"""for var in string:
     print var
+"""
