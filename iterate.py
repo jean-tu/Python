cfbc3c2a8a70cf486eb6af8f57aa195c66bf1b33
Jean Tu <tujean27@gmail.com>
2016-12-12 09:35:07 -0800

    trying to see how to work on the python project

---
+++ iterate.py
@@ -0,0 +1,9 @@
+'''
+string = ['a', b','c','d','e','f','g','h','i','j','k','l','m']
+for var in string:
+    print(var)
+'''
+
+string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,./<>?;:!@#$%^&*()"
+for var in string:
+    print var
